098051cc90aa8373adb771e3ac300a022e755888
Patata <davidromerorubio250@gmail.com>
2025-02-12 16:43:24 +0100

    Practica 1.7

--- js/P1_Escena.js
+++ js/P1_Escena.js
@@ -14,8 +14,9 @@
  * TO DO: Cargar los modulos necesarios
  *******************/
 
-import * as THREE from '../lib/three.module.js';
-import { GLTFLoader } from '../lib/GLTFLoader.js';
+import * as THREE from '/lib/three.module.js';
+import { GLTFLoader } from '/lib/GLTFLoader.js';
+
 
 
 
